dd3fdbe3a49f24586ee9cbe8aa55ed2dbb137350
João Carlos <joaocarlostiads@gmail.com>
2024-10-01 00:10:51 -0300

    chore:Commit Inicial

---
+++ 123Vendas.Domain/Cliente.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _123Vendas.Domain
+{
+    public class Cliente
+    {
+        public int CodigoCliente { get; set; }
+
+        public string NomeCliente { get; set; }
+
+
+    }
+}
---
+++ 123Vendas.Domain/VendaItem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _123Vendas.Domain
+{
+    public class VendaItem
+    {
+        public Produto produto { get; set; }
+
+        public int QuantidadeVenda { get; set; }
+
+        public double TotalDescontos { get; set; }
+
+        public double ValorTotalItem { get; set; }
+
+        public bool ItemCancelado { get; set; }
+
+
+
+
+
+    }
+}
